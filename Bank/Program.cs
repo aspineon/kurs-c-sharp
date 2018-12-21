928ffbb9ca11d7061f5ae4398fe30ef268f1ed61
Marek Zając <contact@zajacmarek.com>
2018-12-21 21:45:39 +0100

    Manager banku

--- Bank/Program.cs
+++ Bank/Program.cs
@@ -10,20 +10,8 @@ namespace Bank
     {
         static void Main(string[] args)
         {
-            AccountsManager manager = new AccountsManager();
-
-            manager.CreateBillingAccount("Marek", "Zajac", 1234567890);
-            manager.CreateSavingsAccount("Marek", "Zajac", 1234567890);
-            manager.CreateSavingsAccount("Aaaaa", "Bbbbb", 0987654321);
-
-            IEnumerable<string> users = manager.ListOfCustomers();
-
-            foreach (string user in users)
-            {
-                Console.WriteLine(user);
-            }
-
-            Console.ReadKey();
+            BankManager bankManager = new BankManager();
+            bankManager.Run();
         }
     }
 }
